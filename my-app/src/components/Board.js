8260fd0eb564aefbd8ccbed6fae8fdef49f0a05f
Jaime J Cobo <jaimecobo@outlook.com>
2021-07-20 13:28:06 -0600

    working with React

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -5,6 +5,36 @@ import Note from './Note';
 class Board extends Component {
   constructor() {
     super();
+    this.state = {
+      notes: []
+    }
+  }
+
+  addNote() {
+    this.state.notes.push(
+      {
+        id: Date.now()
+      }
+    );
+    this.setState(
+      {
+        notes: this.state.notes
+      }
+    );
+  }
+
+  deleteNote(id){
+    let newNoteArr = this.state.notes;
+    newNoteArr.map((note, index) => {
+      if (id === note.id) {
+        newNoteArr.splice(index,1);
+      }
+    });
+    this.setState(
+      {
+        notes: newNoteArr
+      }
+    );
   }
 
   render() {
@@ -12,13 +42,15 @@ class Board extends Component {
       <div>
         <div className="div-board">
           <div className="row">
-            <Note title="Class Notes" body="Always use constructors when extending another class"/>
-            <Note title="My New Address" body="2001 N Lonhill Phoenix, AZ 81234"/>
-            <Note title="React Notes" body="Everything in React is a component"/>
+            {
+              this.state.notes.map(note => {
+                return <Note key={note.id} id={note.id} deleteHandler={this.deleteNote.bind(this)} />
+              })
+            }
           </div>
         </div>
         <div>
-          <button className="btn btn-success add-button">Add</button>
+          <button className="btn btn-success add-button" onClick={this.addNote.bind(this)}>Add</button>
         </div>
       </div>
     );
